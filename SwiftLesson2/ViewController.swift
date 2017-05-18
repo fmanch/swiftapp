0ae2daa66614dedc8b7d13eb78230c09b907b7c9
Sebastián Mancheno <sebastianmancheno@sebastians-imac.local>
2017-05-18 18:17:50 -0500

    Initial Commit

---
+++ SwiftLesson2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLesson2
+//
+//  Created by Sebastián Mancheno on 5/18/17.
+//  Copyright © 2017 Sebastián Mancheno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
